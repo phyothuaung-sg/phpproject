d04c2fb6410ef1511c05ab040edb249774fc83c4
phyothuaung-sg <thukhinglay@gmail.com>
2025-06-08 22:03:48 +0800

    Student list CRUD

---
+++ backend/category/student_delete.php
@@ -0,0 +1,13 @@
+<?php
+include '../../dbconnect.php';
+
+if(isset($_GET['id'])) {
+    $id = $_GET['id'];
+    $stmt = $pdo ->prepare("DELETE FROM students WHERE id = :id");
+    $stmt -> execute ([
+        'id' => $id
+    ]);
+}
+header('Location: student_list.php');
+
+?>
